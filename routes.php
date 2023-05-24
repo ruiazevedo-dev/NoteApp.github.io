965b372c15aa8d87eee989cee51c016ea15aceb8
Rui Azevedo <ruiazevedodev@outlook.com>
2023-05-24 17:34:33 +0100

    deleted unused files

--- routes.php
+++ routes.php
@@ -1,9 +1,5 @@
 <?php
 
-$router->get('/', 'controllers/index.php');
-$router->get('/about', 'controllers/about.php');
-$router->get('/contact', 'controllers/contact.php');
-
 $router->get('/notes', 'controllers/notes/index.php')->only('auth');
 $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
