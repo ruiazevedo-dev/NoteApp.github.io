b2931a453d6c23b3996ccb032ec8a6a1af821dcc
Rui Azevedo <ruiazevedodev@outlook.com>
2023-05-29 18:45:14 +0100

    updated notes index

--- views/notes/index.view.php
+++ views/notes/index.view.php
@@ -3,11 +3,11 @@
 <?php require base_path('views/partials/banner.php') ?>
 
 <main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <ul>
+    <!-- <div class="px-4 sm:px-8 max-w-5xl m-auto">
+        <ul class="border border-gray-200 rounded overflow-hidden shadow-md">
             <?php foreach ($notes as $note) : ?>
                 <li>
-                    <a href="/note?id=<?= $note['id'] ?>" class="text-blue-500 hover:underline">
+                    <a href="/note?id=<?= $note['id'] ?>" class="px-4 py-2 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out">
                         <?= htmlspecialchars($note['body']) ?>
                     </a>
                 </li>
@@ -17,6 +17,25 @@
         <p class="mt-6">
             <a href="/notes/create" class="text-blue-500 hover:underline">Create Note</a>
         </p>
+    </div> -->
+    <div class="mt-6">
+        <div class="px-4 sm:px-8 max-w-3xl m-auto">
+            <ul class="border border-gray-200 rounded overflow-hidden shadow-md">
+                <?php foreach ($notes as $note) : ?>
+                    <li class="px-4 py-2 text-slate-900 bg-white hover:bg-sky-100 hover:text-sky-900 border-b last:border-none border-gray-200 transition-all duration-300 ease-in-out"><a href="/note?id=<?= $note['id'] ?>">
+                            <?= htmlspecialchars($note['body']) ?>
+                        </a></li>
+                <?php endforeach; ?>
+            </ul>
+            <div class="mt-6 grid grid-cols-6 gap-4 ">
+                <p class="col-start-0 col-span-2">
+                    <a href="/notes/create" class="inline-flex items-start justify-center rounded-md border border-transparent bg-blue-400 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Create Note</a>
+                </p>
+
+            </div>
+
+
+        </div>
     </div>
 </main>
 
--- views/partials/head.php
+++ views/partials/head.php
@@ -1,9 +1,12 @@
 <!doctype html>
 <html lang="en" class="h-full bg-gray-100">
+
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.tailwindcss.com?plugins=forms"></script>
+    <!--  -->
 </head>
 
 <body class="h-full">
